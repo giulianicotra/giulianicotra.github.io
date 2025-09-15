7902524371874a79983fd1f95f74281c84c91bf6
giulianicotra <giulianicotra@users.noreply.github.com>
2025-09-15 17:09:57 +0000

    deploy: 3c62eb10f8505b70162eb60c11dfb435d4c67cd6

---
+++ _next/static/chunks/aa1c117112ce7269.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/c320040b5aec9780.js",
+  "static/chunks/turbopack-466bda07218de2fb.js"
+])
---
+++ _next/static/chunks/fc873fe593c1ee4b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/c320040b5aec9780.js",
+  "static/chunks/turbopack-d1eb0f9cf60e0370.js"
+])
